71ab186f70476a312deb391b699f6b61f64fbff5
Niko Todorov <todorov@chapman.edu>
2022-07-20 16:18:56 -0700

    Update pi.c
    compare the two pies :-)

--- code/parallel/ExerciseDay4/ex2/pi.c
+++ code/parallel/ExerciseDay4/ex2/pi.c
@@ -7,17 +7,17 @@ static long int numSteps = 1000000000;
 int main() {
 
   // perform calculation
-  double pi   = 0;
+  double pi = 0; // , time = 0;
   double dx = 1./numSteps;
   double x  = dx*0.50;
-  
+
   for (int i=0; i<numSteps; i++) {
     pi += 4./(1.+x*x);
     x += dx;
   }
-  
+
   pi *= dx;
-  
-  printf("PI = %16.14f Difference from math.h definition %16.14f \n",pi, pi-M_PI);
+
+  printf("PI = %42.40f via 1bil iter OMP\n PI = %42.40f from math.h\n",pi, M_PI); // time
   return 0;
 }
